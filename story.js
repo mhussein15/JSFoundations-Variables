7da8358e5592e8001b167c93b2117e66816c4379
mhussein15 <mhalfakih15@gmail.com>
2021-01-10 12:26:59 +0300

    Add name,age, and hobby

---
+++ story.js
@@ -0,0 +1,8 @@
+const name = "Mohammad Hussein";
+const age = 25;
+const hobby = "programming";
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${
+  age + 33
+}. By then you'll have decades of ${hobby} practice!
+`);
